735a40e452586da46886332baa8559676e74a3fa
DarkJediXP <seanmcdaniel@s-dyn-191-133.dtcc.edu>
2014-04-08 14:18:27 -0400

    Added a list of features under home.php
    Added a list of features under home.php and did further testing. Will
    do one more round of testing before i submit.

--- application/views/home.php
+++ application/views/home.php
@@ -1,4 +1,18 @@
 <p>This is a quick overview of some features.</p>
+<br>
+<li>A user can edit documents</li>
+<br>
+<li>Users can see what documents have been approved</li>
+<br>
+<li>Users with admin privileges can approve documents in the app. </li>
+<br>
+<li>Security for the app is provided by MD5() hashing</li>
+<br>
+<li>User can see the list of docs with paging and sorting functionality</li>
+<br>
+<li>Admins can approve docs under Document Panel on left.</li>
+<br>
+<li>Enjoy!</li>
 
 <?php
 preg_match('/MSIE (.*?);/', $_SERVER['HTTP_USER_AGENT'], $matches);
